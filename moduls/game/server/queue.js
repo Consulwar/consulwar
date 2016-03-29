62a86e9f71e0e36b3d9478f449b78aec0ca2bd03
kitecrimson <kitecrimson@gmail.com>
2016-03-29 19:32:15 +0200

    Save created time at queue task for debug

--- moduls/game/server/queue.js
+++ moduls/game/server/queue.js
@@ -37,7 +37,8 @@ Game.Queue.add = function(item) {
 		status: 0,
 		type: item.type,
 		startTime: startTime,
-		finishTime: startTime + item.time
+		finishTime: startTime + item.time,
+		createdTime: Game.getCurrentTime() // debug field
 	}
 
 	var select = {

47e9a055c7406c3de4e40117ba089bf5eab89dff
Alexandr Zelenin <zav.work@gmail.com>
2016-04-10 05:06:31 +0200

    Tooltips text small adjustment

--- moduls/game/client/helper.js
+++ moduls/game/client/helper.js
@@ -291,6 +291,10 @@ var getEffectsTooltip = function(price, effects, target, invert, side) {
 		prevPriority = priority;
 	}
 
+	if (effectsValues.length && effectsValues[effectsValues.length - 1].total) {
+		effectsValues[effectsValues.length - 1].final = true;
+	}
+
 	return {
 		'data-tooltip': Blaze.toHTMLWithData(Template.tooltipTable, {
 			values: effectsValues

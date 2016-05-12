7f70bb0fe1867913dddafe2d95d6f122f5577fc8
kitecrimson <kitecrimson@gmail.com>
2016-05-12 16:59:14 +0200

    #36: Achievements

--- moduls/achievements/lib/achievements.js
+++ moduls/achievements/lib/achievements.js
@@ -1,14 +1,32 @@
 initAchievementsLib = function() {
 
 game.Achievement = function(options) {
+	this.doNotRegisterEffect = true;
 
+	game.Achievement.superclass.constructor.apply(this, arguments);
+
+	this.type = 'achievement';
+	this.currentLevel = options.currentLevel;
+
+	if (Game.Achievements.items[options.engName]) {
+		throw new Meteor.Error('Ошибка в контенте', 'Дублируется достижение ' + options.engName);
+	}
+
+	Game.Achievements.items[options.engName] = this;
 };
+game.extend(game.Achievement, game.Item);
 
 Game.Achievements = {
 	items: {},
 
 	getCompleted: function() {
-		return [];
+		var result = [];
+		for (var key in Game.Achievements.items) {
+			if (Game.Achievements.items[key].currentLevel() > 0) {
+				result.push(Game.Achievements.items[key]);
+			}
+		}
+		return result;
 	}
 };
 

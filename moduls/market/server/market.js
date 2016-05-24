ff833983f0bf58c9c30e6500daea9efff580ca43
kitecrimson <kitecrimson@gmail.com>
2016-05-24 12:33:35 +0200

    Market fix

--- moduls/market/server/market.js
+++ moduls/market/server/market.js
@@ -16,7 +16,10 @@ Meteor.methods({
 
 		var userResources = Game.Resources.getValue();
 
-		if (!userResources[resourceFrom] || userResources[resourceFrom].amount < amount) {
+		if (!userResources[resourceFrom]
+		 || !userResources[resourceFrom].amount
+		 ||  userResources[resourceFrom].amount < amount
+		) {
 			throw new Meteor.Error('Недостаточно ресурсов');
 		}
 

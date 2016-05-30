d80c9049d34e1577c4dfb52cb63ba250cf1c8820
kitecrimson <kitecrimson@gmail.com>
2016-05-30 14:27:04 +0200

    DDP Limiter config

--- moduls/game/server/ddplimiter.js
+++ moduls/game/server/ddplimiter.js
@@ -1,5 +1,17 @@
 initDDPLimiter = function() {
 
+if (!Meteor.settings.ddplimiter
+ ||  Meteor.settings.ddplimiter.isEnabled === undefined
+ || !Meteor.settings.ddplimiter.numRequests
+ || !Meteor.settings.ddplimiter.timeInterval
+) {
+	throw new Meteor.Error('Ошибка в настройках', 'Заполни параметры модуля DDPLimiter (см. settings.sample ddplimiter)');
+}
+
+var IS_ENABLED = Meteor.settings.ddplimiter.isEnabled;
+var NUM_REQUESTS = Meteor.settings.ddplimiter.numRequests;
+var TIME_INTERVAL = Meteor.settings.ddplimiter.timeInterval;
+
 var HEAVY_METHODS = [
 	'actualizeGameInfo',
 	'getBonusResources',
@@ -13,16 +25,18 @@ var HEAVY_METHODS = [
 	'chat.sendMessage'
 ];
 
-DDPRateLimiter.addRule({
-	type: 'method',
-	name: function(methodName) {
-		return true;
-		// Only for methods in array
-		// return HEAVY_METHODS.indexOf(methodName) != -1;
-	},
-	userId: function(userId) {
-		return true;
-	}
-}, 5, 10000);
+if (IS_ENABLED) {
+	DDPRateLimiter.addRule({
+		type: 'method',
+		name: function(methodName) {
+			return true;
+			// Only for methods in array
+			// return HEAVY_METHODS.indexOf(methodName) != -1;
+		},
+		userId: function(userId) {
+			return true;
+		}
+	}, NUM_REQUESTS, TIME_INTERVAL);
+}
 
 };

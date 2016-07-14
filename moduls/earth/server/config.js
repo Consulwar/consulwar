d570f566bb68cd909377faa5e7fa857cf4e97d91
Alexandr Zelenin <zav.work@gmail.com>
2016-07-14 11:53:54 +0200

    Earth units amount and damage reduction

--- moduls/earth/server/config.js
+++ moduls/earth/server/config.js
@@ -3,11 +3,15 @@ initEarthConfigServer = function() {
 	if (!Meteor.settings.earth
 	 || !Meteor.settings.earth.reinforcementsDelay
 	 || !Meteor.settings.earth.minActivePlayers
+	 || !Meteor.settings.earth.spawnCoefficient
+	 || !Meteor.settings.earth.damageReduction
 	) {
 		throw new Meteor.Error('Ошибка в настройках', 'Заполни параметры боев на земле (см. settings.sample earth)');
 	}
 
 	Game.Earth.REINFORCEMENTS_DELAY = Meteor.settings.earth.reinforcementsDelay;
 	Game.Earth.MIN_ACTIVE_PLAYERS = Meteor.settings.earth.minActivePlayers;
+	Game.Earth.SPAWN_COEFFICIENT = Meteor.settings.earth.spawnCoefficient;
+	Game.Earth.DAMAGE_REDUCTION = Meteor.settings.earth.damageReduction;
 
 };

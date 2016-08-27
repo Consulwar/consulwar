0b2fa91f9f4f935c8c116b20ff330a25b025bd6f
dreamniker <dreamniker@gmail.com>
2016-08-28 00:11:17 +0300

    CSWE-59: исправил lookup

--- moduls/game/client/helper.js
+++ moduls/game/client/helper.js
@@ -78,7 +78,10 @@ UI.registerHelper('makeObject', function() {
 
 UI.registerHelper('lookup', function(obj) {
 	var answer = obj;
-	for(var i = 1; (i < arguments.length - 1) && (arguments[i] !== undefined); i++) {
+	for(var i = 1; i < (arguments.length - 1); i++) {
+		if (arguments[i] !== undefined) {
+			return undefined;
+		}
 		answer = answer && answer[arguments[i]];
 	}
 	return answer || undefined;

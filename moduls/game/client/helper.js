d22508bb9fb21c6d6fd7309a8a0dcf3e9839f715
dreamniker <dreamniker@gmail.com>
2016-08-29 04:51:31 +0300

    CSWE-59: исправил хелпер declension

--- moduls/game/client/helper.js
+++ moduls/game/client/helper.js
@@ -88,15 +88,15 @@ UI.registerHelper('lookup', function(obj) {
 });
 
 UI.registerHelper('declension', function(number, zeroForm, singleForm, twoForm, manyForm) {
-	return zeroForm + (
-		(/[0,2-9]?[1]$/.test(number))
-		? singleForm
-		: (
-			(/[0,2-9]?[2-4]$/.test(number))
-			? twoForm
-			: manyForm
-		)
-	);
+    return zeroForm + (
+        (/([0,2-9][1]$)|(^1$)/.test(number))
+        ? singleForm
+        : (
+            (/([0,2-9][2-4]$)|(^[2-4]$)/.test(number))
+            ? twoForm
+            : manyForm
+        )
+    );
 });
 
 UI.registerHelper('formatDate', Game.Helpers.formatDate);

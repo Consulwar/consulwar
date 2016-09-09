25b3eb803f9c77660bca8140385d09680702bae1
dreamniker <dreamniker@gmail.com>
2016-09-10 02:51:21 +0300

    CSWE-12: попап теперь не осткакивает вниз а появляется под курсором

--- moduls/chat/client/chat.js
+++ moduls/chat/client/chat.js
@@ -747,10 +747,10 @@ Template.chat.events({
 		if (!username || username.length <= 0) {
 			return;
 		}
-
+		var chatOffset = t.$('.chat').offset();
 		Game.Chat.showUserPopup(
-			e.pageX,
-			e.pageY - 40,
+			e.pageX - chatOffset.left + 2,
+			e.pageY - chatOffset.top + 2,
 			username
 		);
 	},

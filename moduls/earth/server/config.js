5d2dd36734dad5b047f23ac3282b101e592a48d5
Alexandr Zelenin <zav.work@gmail.com>
2016-09-11 19:47:29 +0200

    Move earth enemies spawn amounts to config

--- moduls/earth/server/config.js
+++ moduls/earth/server/config.js
@@ -5,6 +5,7 @@ initEarthConfigServer = function() {
 	 || !Meteor.settings.earth.minActivePlayers
 	 || !Meteor.settings.earth.spawnCoefficient
 	 || !Meteor.settings.earth.damageReduction
+	 || !Meteor.settings.earth.spawn
 	) {
 		throw new Meteor.Error('Ошибка в настройках', 'Заполни параметры боев на земле (см. settings.sample earth)');
 	}
@@ -13,5 +14,6 @@ initEarthConfigServer = function() {
 	Game.Earth.MIN_ACTIVE_PLAYERS = Meteor.settings.earth.minActivePlayers;
 	Game.Earth.SPAWN_COEFFICIENT = Meteor.settings.earth.spawnCoefficient;
 	Game.Earth.DAMAGE_REDUCTION = Meteor.settings.earth.damageReduction;
+	Game.Earth.SPAWN = Meteor.settings.earth.spawn;
 
 };

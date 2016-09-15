ef7cabec75e28a5bbb35f98437ed432681495da1
dreamniker <dreamniker@gmail.com>
2016-09-15 13:44:06 +0300

    CSWE-79: при первой отрисовке игры в Template.game.onRendered показывается квест из цепочки tutorial если он есть

--- moduls/game/client/main.js
+++ moduls/game/client/main.js
@@ -367,6 +367,15 @@ var helpers = {
 Template.game.helpers(helpers);
 Template.item.helpers(helpers);
 
+Template.game.onRendered(function(){
+	var currentQuest = Game.Quest.getOneByHero('tamily');
+	if (currentQuest
+	 && currentQuest.engName == 'tutorial'
+	 && currentQuest.status == Game.Quest.status.PROMPT
+	) {
+		Game.Quest.showQuest('tutorial');
+	}
+});
 
 Template.game.events({
 	'click header .username .edit': function() {
@@ -534,5 +543,5 @@ Template.inputWindow.events({
 		closeInputWindow(t.data.onAccept, t.find('input').value);
 	}
 });
-
+console.log('main2');
 });

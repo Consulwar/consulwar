b55485aea03ac825a1782a5542831c9c800f0020
Vladimir Karasev <mrkitan@gmail.com>
2017-03-30 17:43:59 +0300

    CSWE-149: fix more accurate default arguments and braces

--- moduls/mutual/lib/beginnerBoost.js
+++ moduls/mutual/lib/beginnerBoost.js
@@ -3,20 +3,19 @@ initBeginnerBoostLib = function () {
 
 const SEC_PER_DAY = 1000 * 60 * 60 * 24;
 
+let getServerDays = function() {
+	let serverDate = new Date(Game.getCurrentServerTime() * 1000);
+	return Math.floor((serverDate - Game.BeginnerBoost.SERVER_START_DATE) / SEC_PER_DAY);
+};
+
+let getUserDays = function() {
+	let userRegisterDate = Meteor.user().createdAt;
+	let updatedDate = new Date(Game.Resources.getValue().updated * 1000);
+	return Math.floor((updatedDate - userRegisterDate) / SEC_PER_DAY);
+};
+
 Game.BeginnerBoost = {
-	calculatePower: function (affectName, serverDays, userDays) {
-		if (serverDays === undefined) {
-			let serverDate = new Date(Game.getCurrentServerTime() * 1000);
-			serverDays = Math.floor((serverDate - Game.BeginnerBoost.SERVER_START_DATE) / SEC_PER_DAY);
-		}
-
-		if (userDays === undefined) {
-			let userRegisterDate = Meteor.user().createdAt;
-			let updatedDate = new Date(Game.Resources.getValue().updated * 1000);
-			userDays = Math.floor((updatedDate - userRegisterDate) / SEC_PER_DAY);
-		}
-
-
+	calculatePower: function (affectName, serverDays = getServerDays(), userDays = getUserDays()) {
 		let power = this.calculateGrowth(serverDays) - this.calculateDecrease(userDays);
 		return Math.max(0, power) * Game.BeginnerBoost.POWER_UNIT[affectName];
 	},
@@ -26,8 +25,7 @@ Game.BeginnerBoost = {
 		if (days <= growth.initial.days) {
 			return (days / growth.initial.interval) * growth.initial.power;
 		} else {
-			const growthMax = (growth.initial.days / growth.initial.interval *
-				growth.initial.power);
+			const growthMax = (growth.initial.days / growth.initial.interval) * growth.initial.power;
 
 			return growthMax + ((days - growth.initial.days) / growth.continuous.interval) *
 				growth.continuous.power;
@@ -39,8 +37,7 @@ Game.BeginnerBoost = {
 		if (days <= decrease.initial.days) {
 			return (days / decrease.initial.interval) * decrease.initial.power;
 		} else {
-			const decreaseMax = (decrease.initial.days / decrease.initial.interval *
-				decrease.initial.power);
+			const decreaseMax = (decrease.initial.days / decrease.initial.interval) * decrease.initial.power;
 
 			return decreaseMax + ((days - decrease.initial.days) / decrease.continuous.interval) *
 				decrease.continuous.power;

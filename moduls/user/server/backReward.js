7dde19569d11702afe527baf996a3661c49122e5
Koroffin <n.koroffin@gmail.com>
2017-03-31 09:52:34 +0300

    CSWE-147: fix getProfit

--- moduls/user/server/backReward.js
+++ moduls/user/server/backReward.js
@@ -2,25 +2,22 @@ initBackRewardServer = function() {
 
 'use strict';
 
-Game.BackReward = function() {
+Game.BackReward = {
+	getProfit: function() {
+		let backTime = Game.getCurrentTime() - Game.Resources.getValue().updated;
+		let Rewards = Game.Cards.items['backReward'];
+		let reward;
 
-};
-
-
-Game.BackReward.getProfit = function() {
-	let backTime = Game.getCurrentTime() - Game.Resources.getValue().updated;
-	let Rewards = Game.Cards.items['backReward'];
-	let reward;
-
-	for (var key in Rewards) {
-		let r = Rewards[key];
-		let fromDay = r.fromDay * 24 * 3600;
-		if ((fromDay <= backTime) && (!reward || (reward.fromDay * 24 * 3600 < fromDay))) {
-			reward = r;
+		for (var key in Rewards) {
+			let r = Rewards[key];
+			let fromDay = r.fromDay * 24 * 3600;
+			if ((fromDay <= backTime) && (!reward || (reward.fromDay * 24 * 3600 < fromDay))) {
+				reward = r;
+			}
 		}
-	}
 
-	return reward;
+		return reward;		
+	}
 };
 
 Meteor.methods({

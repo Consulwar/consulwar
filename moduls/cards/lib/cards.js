e9723c291bcf66505a2dda899938ec2cc04c97f3
Koroffin <n.koroffin@gmail.com>
2017-04-01 12:42:45 +0300

    CSWE-147: fix

--- moduls/cards/lib/cards.js
+++ moduls/cards/lib/cards.js
@@ -88,12 +88,7 @@ game.Card = function(options) {
 game.extend(game.Card, game.Item);
 
 game.BackRewardCard = function(options) {
-	// Параметры по умолчанию, чтобы не дублировать в контенте
-	options.cardType = 'backReward';
-	options.cardGroup = 'backRewards';
-
 	game.BackRewardCard.superclass.constructor.apply(this, arguments);
-
 	this.fromDay = options.fromDay;
 };
 game.extend(game.BackRewardCard, game.Card);

fab80a3ab7e2372bc315f601ec1cbbb3b037146d
Vladimir Karasev <mrkitan@gmail.com>
2017-04-03 13:36:15 +0300

    CSWE-147: restructure if to begin of function

--- moduls/user/server/backReward.js
+++ moduls/user/server/backReward.js
@@ -5,20 +5,23 @@ initBackRewardServer = function() {
 Game.BackReward = {
 	getReward: function() {
 		let daysFromUpdate = (Game.getCurrentTime() - Game.Resources.getValue().updated) / (24 * 3600);
+
+		if (daysFromUpdate < Game.BackRewardCard.MIN_REWARD_DAY) {
+			return;
+		}
+
 		let cards = Game.Cards.items['backReward'];
 		let applicableCard;
-		
-		if (daysFromUpdate >= Game.BackRewardCard.MIN_REWARD_DAY) {
-			for (let key in cards) {
-				if (cards.hasOwnProperty(key)) {
-					let card = cards[key];
-					if ((card.fromDay <= daysFromUpdate) && (!applicableCard || (applicableCard.fromDay < card.fromDay))) {
-						applicableCard = card;
-					}
+
+		for (let key in cards) {
+			if (cards.hasOwnProperty(key)) {
+				let card = cards[key];
+				if ((card.fromDay <= daysFromUpdate) && (!applicableCard || (applicableCard.fromDay < card.fromDay))) {
+					applicableCard = card;
 				}
 			}
-			Game.Cards.activate(applicableCard, Meteor.user());
 		}
+		Game.Cards.activate(applicableCard, Meteor.user());
 	}
 };
 

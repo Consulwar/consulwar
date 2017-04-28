2dc5e051e599a49686313e2c88921b77c3cb7be2
Vladimir Karasev <mrkitan@gmail.com>
2017-04-28 17:49:36 +0300

    CSWE-210: add settings for disable datadog

--- moduls/game/server/datadog.js
+++ moduls/game/server/datadog.js
@@ -1,27 +1,38 @@
 initDataDog = function() {
 'use strict';
 
-if (!Meteor.settings.datadog || !Meteor.settings.datadog.host || !Meteor.settings.datadog.port) {
-	throw new Meteor.Error('Ошибка в настройках', 'Заполни параметры модуля DataDog (см. settings.sample datadog)');
-}
+if (!Meteor.settings.datadog || !Meteor.settings.datadog.isEnabled) {
 
-let HOST = Meteor.settings.datadog.host;
-let PORT = Meteor.settings.datadog.port;
+	console.log('DataDog выключен!');
 
-let StatsD = require('node-dogstatsd').StatsD;
-Game.datadog = new StatsD(HOST, PORT);
+	Game.datadog = {
+		increment: function() {},
+		set: function() {}
+	};
 
-
-SyncedCron.add({
-	name: 'Сохранение в метрики онлайн и всего зарегистрированных',
-	schedule: function(parser) {
-		return parser.text('every 5 mins');
-	},
-	job: function() {
-		Game.datadog.set('user.totalUsersCount', Meteor.users.find().count());
-
-		Game.datadog.set('user.onlineUsersCount', Meteor.users.find({'status.online': true}).count());
+} else {
+	if (!Meteor.settings.datadog || !Meteor.settings.datadog.host || !Meteor.settings.datadog.port) {
+		throw new Meteor.Error('Ошибка в настройках', 'Заполни параметры модуля DataDog (см. settings.sample datadog)');
 	}
-});
 
+	let HOST = Meteor.settings.datadog.host;
+	let PORT = Meteor.settings.datadog.port;
+
+	let StatsD = require('node-dogstatsd').StatsD;
+	Game.datadog = new StatsD(HOST, PORT);
+
+
+	SyncedCron.add({
+		name: 'Сохранение в метрики онлайн и всего зарегистрированных',
+		schedule: function(parser) {
+			return parser.text('every 5 mins');
+		},
+		job: function() {
+			Game.datadog.set('user.totalUsersCount', Meteor.users.find().count());
+
+			Game.datadog.set('user.onlineUsersCount', Meteor.users.find({'status.online': true}).count());
+		}
+	});
+
+}
 };

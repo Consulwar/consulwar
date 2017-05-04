872b26522464da6168da01e576c2b0b0ea7342ec
Alexandr Zelenin <zav.work@gmail.com>
2017-05-05 01:04:31 +0200

    Update datadog metrics

--- moduls/game/server/datadog.js
+++ moduls/game/server/datadog.js
@@ -13,31 +13,41 @@ if (!Meteor.settings.datadog || !Meteor.settings.datadog.isEnabled) {
 	if (!Meteor.settings.datadog
 		|| !Meteor.settings.datadog.host
 		|| !Meteor.settings.datadog.port
-		|| !Meteor.settings.datadog.prefix
+		|| Meteor.settings.datadog.prefix !== undefined
 	) {
 		throw new Meteor.Error('Ошибка в настройках', 'Заполни параметры модуля DataDog (см. settings.sample datadog)');
 	}
 
-	let HOST = Meteor.settings.datadog.host;
-	let PORT = Meteor.settings.datadog.port;
-	let PREFIX = Meteor.settings.datadog.prefix;
+	let host = Meteor.settings.datadog.host;
+	let port = Meteor.settings.datadog.port;
+	let prefix = Meteor.settings.datadog.prefix;
 
 	let StatsD = require('hot-shots');
-	Game.datadog = new StatsD(HOST, PORT, PREFIX);
+	Game.datadog = new StatsD({
+		host, 
+		port, 
+		prefix,
+		globalTags: [`pmId:${process.env.pm_id}`]
+	});
 
 	Game.datadog.event('Process started', `processId: ${Game.processId}, pmId: ${process.env.pm_id}`);
 
-	SyncedCron.add({
-		name: 'Сохранение в метрики онлайн и всего зарегистрированных',
-		schedule: function(parser) {
-			return parser.text('every 20 sec');
-		},
-		job: function() {
-			Game.datadog.gauge('user.totalUsersCount', Meteor.users.find().count());
+	let sendOnlineData = function() {
+		Game.datadog.gauge(
+			'user.connectedNotAuthorized', 
+			UserStatus.connections.find({userId: {$exists: false}}).count()
+		);
+		Game.datadog.gauge(
+			'user.connectedAuthorized', 
+			UserStatus.connections.find({userId: {$exists: true}}).count()
+		);
 
-			Game.datadog.gauge('user.onlineUsersCount', Meteor.users.find({'status.online': true}).count());
-		}
-	});
+		Game.datadog.gauge('user.total', Meteor.users.find().count());
+		Game.datadog.gauge('user.online', Meteor.users.find({'status.online': true}).count());
+	}
 
+	UserStatus.events.on('connectionLogin', sendOnlineData);
+	UserStatus.events.on('connectionLogout', sendOnlineData);
 }
+
 };

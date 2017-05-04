193b0cbf996c96f19aad54fd4c8e944b4a7ee744
Vladimir Karasev <mrkitan@gmail.com>
2017-05-04 11:59:50 +0300

    CSWE-210: add date formatting

--- moduls/game/server/game_log.js
+++ moduls/game/server/game_log.js
@@ -4,8 +4,28 @@ initGameLog = function() {
 initDataDog();
 
 Game.Log = function(desc, info = Meteor.user().username) {
-	console.log(new Date() + ' ' + info + ': ' + desc);
+	let now = new Date();
+	let dateStr = now.getFullYear() + '.' + pad(now.getMonth() + 1) + '.' + pad(now.getDate()) + ' ' +
+		pad(now.getHours()) + ':' + pad(now.getMinutes()) + ':' + pad(now.getSeconds()) + '.' +
+		pad1000(now.getMilliseconds());
+
+	console.log('[' + dateStr + '] ' + info + ': ' + desc);
 
 	Game.datadog.increment(desc);
 };
+
+let pad = function(value) {
+	return (value < 10) ? '0' + value : value;
+};
+
+let pad1000 = function(value) {
+	if (value < 10) {
+		return '00' + value;
+	} else if (value < 100) {
+		return '0' + value;
+	} else {
+		return value;
+	}
+};
+
 };

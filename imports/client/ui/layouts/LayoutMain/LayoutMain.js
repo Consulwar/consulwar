3357e59a5e0bf04ae3a4db341eba01da05f847ef
Vadim Lihachev <dcat.design@gmail.com>
2018-02-12 18:58:47 +0400

    CSWE-296: Created Main layout and page Index

---
+++ imports/client/ui/layouts/LayoutMain/LayoutMain.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { BlazeComponent } from 'meteor/peerlibrary:blaze-components';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Router } from 'meteor/iron:router';
+import { $ } from 'meteor/jquery';
+import Game from '/moduls/game/lib/main.game';
+import '/imports/client/ui/button/button.styl';
+import './LayoutMain.html';
+import './LayoutMain.styl';
+
+if (!Meteor.settings.public.isInviteRequired) {
+  reCAPTCHA.config({
+    publickey: Meteor.settings.public.recaptcha.publickey,
+    hl: 'ru',
+  });
+}
+
+class LayoutMain extends BlazeComponent {
+  template() {
+    return 'LayoutMain';
+  }
+
+  onCreated() {
+    super.onCreated();
+
+    this.registered = new ReactiveVar('…');
+    this.online = new ReactiveVar('…');
+
+    Meteor.call('totalUsersCount', (err, count) => this.registered.set(count));
+    Meteor.call('onlineUsersCount', (err, count) => this.online.set(count));
+  }
+
+  onRendered() {
+    $(this.find('.scrollbar-inner')).perfectScrollbar();
+  }
+
+  currentRouteName() {
+    return Router.current().route.getName();
+  }
+}
+
+LayoutMain.register('LayoutMain');
+
+export default LayoutMain;

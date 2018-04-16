72213d4a32f40ce08a38cf69e6659170888fc8d3
Vadim Lihachev <dcat.design@gmail.com>
2018-04-17 00:33:45 +0400

    CSWE-324_buffs: Buffs - remake to vertical icons

--- imports/client/ui/blocks/Buffs/Buffs.js
+++ imports/client/ui/blocks/Buffs/Buffs.js
@@ -9,6 +9,13 @@ class Buffs extends BlazeComponent {
     return 'Buffs';
   }
 
+  getEffectObj(effect) {
+    return {
+      type: (effect.affect && effect.affect.toString()) || '',
+      effectText: `${effect.textBefore}${effect.result()}${effect.textAfter}`,
+    };
+  }
+
   getBonus() {
     const activeBonuses = Game.Cards.getActive();
     const bonusList = [];
@@ -17,20 +24,41 @@ class Buffs extends BlazeComponent {
       const bonus = {
         name: item.name,
         type: item.engName,
-        time: (item.getActiveTask().finishTime - Game.getCurrentServerTime()),
+        effectsHtml: '',
+        hasMark: false,
       };
 
+      const time = item.getActiveTask().finishTime - Game.getCurrentServerTime();
+      bonus.time = Game.Helpers.formatSeconds(time);
+
       if (item.effects.length > 1) {
-        let tooltip = '';
         _.forEach(item.effects, (effectItem) => {
-          tooltip += `${effectItem.textBefore}${effectItem.result()}${effectItem.textAfter}<br/>`;
+          bonus.effectsHtml += `${this.getEffectObj(effectItem).effectText}<br/>`;
         });
-        bonus.tooltip = tooltip;
       } else {
-        bonus.effect = item.effects;
-        bonus.type = item.effects[0].affect.toString();
-        bonus.tooltip = item.name;
+        bonus.name = this.getEffectObj(item.effects[0]).effectText;
+        bonus.type = this.getEffectObj(item.effects[0]).type;
+        bonus.hasMark = true;
       }
+
+      const tooltipTemplate = _.template(`
+        <div
+          class="
+            cw--Buffs__tooltipHeader
+            cw--Buffs__tooltipHeader_<%- type %>
+          "
+        >
+          <%- name %>
+          <time class="cw--Buffs__timer">
+            <%- time %>
+          </time>
+        </div>
+        <div class="cw--Buffs__effects">
+          <%= effectsHtml %>
+        </div>
+      `);
+
+      bonus.tooltip = tooltipTemplate(bonus);
 
       bonusList.push(bonus);
     });

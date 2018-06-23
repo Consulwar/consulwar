4ba3345cb29fceb1658921b156d93e62b55120b2
Vadim Lihachev <dcat.design@gmail.com>
2018-06-23 13:39:59 +0400

    CSWE-349: Sort consuls - shorter way

--- imports/client/ui/blocks/Earth/Consuls/EarthConsuls.js
+++ imports/client/ui/blocks/Earth/Consuls/EarthConsuls.js
@@ -44,16 +44,7 @@ class EarthConsuls extends BlazeComponent {
         if (err) {
           Notifications.error('Не удалось получить данные о точке', err.error);
         } else {
-          const sortUsersByPower = (a, b) => {
-            if (a.power > b.power) {
-              return -1;
-            }
-            if (a.power < b.power) {
-              return 1;
-            }
-            return 0;
-          };
-          this.consuls.set(users.sort(sortUsersByPower));
+          this.consuls.set(users.sort((a, b) => b.power - a.power));
           this.isLoading.set(false);
         }
       },

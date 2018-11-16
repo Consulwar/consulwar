aad060bdfb6ac4becd22459059210fc5f8e264f3
Vasiliy Halimonchuk <vasiliy.halimonchuk@gmail.com>
2018-11-16 11:01:35 +0200

    "Event master" achievement added

--- imports/content/Achievement/Communication/lib/index.js
+++ imports/content/Achievement/Communication/lib/index.js
@@ -1,7 +1,9 @@
 import Seppuku from './Seppuku';
 import Winner from './Winner';
+import EventMaster from './EventMaster';
 
 export default {
   'Achievement/Communication/Seppuku': Seppuku,
   'Achievement/Communication/Winner': Winner,
+  'Achievement/General/EventMaster': EventMaster,
 };

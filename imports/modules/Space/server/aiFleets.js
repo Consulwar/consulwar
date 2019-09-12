a0735cd1bb3cee321f14d71e849e95320a723fce
Alexandr Zelenin <zav.work@gmail.com>
2019-09-12 23:38:21 +0400

    Ability to spawn council fleets by admin

--- imports/modules/Space/server/aiFleets.js
+++ imports/modules/Space/server/aiFleets.js
@@ -1,9 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import { SyncedCron } from 'meteor/percolate:synced-cron';
 import { _ } from 'meteor/underscore';
 import Game from '/moduls/game/lib/main.game';
+import User from '/imports/modules/User/server/User';
 import Utils from '/imports/modules/Space/lib/utils';
+import Log from '/imports/modules/Log/server/Log';
 import systemUser from '/moduls/user/server/systemUser';
+import humanSpaceUnits from '/imports/content/Unit/Human/Space/server';
 import Space from '../lib/space';
 import Hex from '../../MutualSpace/lib/Hex';
 import mutualSpaceCollection from '../../MutualSpace/lib/collection';
@@ -27,7 +31,13 @@ const findClosestHex = function(target) {
   ));
 };
 
-const spawnAiFleet = function() {
+const spawnAiFleet = function({
+  missionType = 'patrolfleet',
+  missionLevel = { $gte: 1, $lte: 2 },
+  fleet = {
+    'Unit/Human/Space/Mirage': 25,
+  },
+} = {}) {
   if (greenHexes.length === 0) {
     loadGreenHexes();
   }
@@ -46,8 +56,8 @@ const spawnAiFleet = function() {
     {
       $match: {
         username: randomHex.username,
-        'mission.type': 'patrolfleet',
-        'mission.level': { $gte: 1, $lte: 2 },
+        'mission.type': missionType,
+        'mission.level': missionLevel,
       },
     },
     { $sample: { size: 1 } },
@@ -73,9 +83,7 @@ const spawnAiFleet = function() {
   const startPosition = (new Hex(closestHex).center());
 
   const newArmyId = Game.Unit.createArmy(
-    {
-      'Unit/Human/Space/Mirage': 25,
-    },
+    fleet,
     Game.Unit.location.SHIP,
     systemUser._id,
   );
@@ -132,3 +140,56 @@ if (Meteor.settings.last) {
     },
   });
 }
+
+const declension = function(number, zeroForm, singleForm, twoForm, manyForm) {
+  let ending = '';
+  if (/^(.*[0,2-9])?[1]$/.test(number)) {
+    ending = singleForm;
+  } else if (/^(.*[0,2-9])?[2-4]$/.test(number)) {
+    ending = twoForm;
+  } else {
+    ending = manyForm;
+  }
+  return `${zeroForm}${ending}`;
+};
+
+Meteor.methods({
+  'aiFleets.spawn'(options = {
+    count: 1,
+  }) {
+    const user = User.getById();
+    User.checkAuth({ user });
+
+    if (user.role !== 'admin') {
+      throw new Meteor.Error('Админамана нада!');
+    }
+
+    check(options, Object);
+
+    const {
+      count,
+      missionType,
+      missionLevel,
+      fleet,
+    } = options;
+
+    check(count, Number);
+
+    if (fleet && _(fleet).keys().some(id => (!humanSpaceUnits[id]) || fleet[id] < 1)) {
+      throw new Meteor.Error('Косячный флот');
+    }
+
+    Log.method.call(this, { name: 'aiFleets.spawn', user });
+
+    for (let i = 0; i < count; i += 1) {
+      spawnAiFleet({
+        missionType,
+        missionLevel,
+        fleet,
+      });
+    }
+
+    const message = `Совет выслал в поддержку ${count} ${declension(count, 'флот', '', 'а', 'ов')}`;
+    Game.Broadcast.add(message);
+  },
+});
